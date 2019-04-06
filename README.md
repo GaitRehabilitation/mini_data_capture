b741aae4a1b871a1108fb94e93d37b6dbd3308d4
Michael Pollind <polli104@mail.chapman.edu>
2019-04-06 13:07:57 -0700

    added fuse configurations

--- README.md
+++ README.md
@@ -10,6 +10,9 @@
 
 ```avrdude -c <programmer> -p atmega328 -b 57600  -U flash:w:main.hex -U lfuse:w:0xff:m -U hfuse:w:0xd1:m -U efuse:w:0xff:m```
 
+
+![avr fuse](./fuse_bits.png)
+
 ## Maintainers
 
 The current maintainers of this repository are:
--- app/FileDecoder.cpp
+++ app/FileDecoder.cpp
@@ -15,12 +15,13 @@
 // https://www.invensense.com/products/motion-tracking/9-axis/icm-20948/
 //#define ACCEL_SENSIVITY 16384 // +-2g
 #define ACCEL_SENSIVITY 8192 // +-4g
-//#define ACCEL_SENSIVITY 4096 // +-8g
+//#define ACCEL_SENSIVITY 4096
+// +-8g
 //#define ACCEL_SENSIVITY 2048 // +-16g
 
 //#define GYRO_SENSIVITY 131  // +-250  deg/s
-#define GYRO_SENSIVITY 65.5 // +-500  deg/s
-//#define GYRO_SENSIVITY 32.8 // +-1000 deg/s
+//#define GYRO_SENSIVITY 65.5 // +-500  deg/s
+#define GYRO_SENSIVITY 32.8 // +-1000 deg/s
 //#define GYRO_SENSIVITY 16.4 // +-2000 deg/s
 
 
@@ -48,38 +49,39 @@ void FileDecoder::run() {
     while (file.read(payload,512) > 0){
         int idx = 0;
         short count =
-                ((unsigned char)payload[idx + 1] << 8) |
-                ((unsigned char)payload[idx + 0]);
+                short(((unsigned char)payload[idx + 1] << 8) |
+                ((unsigned char)payload[idx]));
         if(count == 0)
             break;
         qDebug() << count;
         idx += 2;
+        idx += 2;
         for(int i = 1; i <= count; ++i){
-            auto time = (unsigned int)(((unsigned char)payload[idx + 3] << 24) |
+            auto time = (unsigned int)(((unsigned char)payload[idx + 3 ] << 24) |
                                 ((unsigned char)payload[idx + 2] << 16) |
                                 ((unsigned char)payload[idx + 1] << 8) |
-                                ((unsigned char)payload[idx]));
+                                ((unsigned char)payload[idx + 0]));
             idx += 4;
-            auto accx = short(((unsigned char)payload[idx + 3] << 8) |
-                         ((unsigned char)payload[idx + 2]));
+            auto accx = short(((unsigned char)payload[idx+1] << 8) |
+                         ((unsigned char)payload[idx]));
             idx += 2;
-            auto accy = short(((unsigned char)payload[idx + 3] << 8) |
-                               ((unsigned char)payload[idx + 2]));
+            auto accy = short(((unsigned char)payload[idx + 1] << 8) |
+                               ((unsigned char)payload[idx ]));
             idx += 2;
-            auto accz = short(((unsigned char)payload[idx + 3] << 8) |
-                               ((unsigned char)payload[idx + 2]));
+            auto accz = short(((unsigned char)payload[idx + 1] << 8) |
+                               ((unsigned char)payload[idx]));
             idx += 2;
-            auto gyrox = short(((unsigned char)payload[idx + 3] << 8) |
-                               ((unsigned char)payload[idx + 2]));
+            auto gyrox = short(((unsigned char)payload[idx + 1] << 8) |
+                               ((unsigned char)payload[idx ]));
             idx += 2;
-            auto gyroy = short(((unsigned char)payload[idx + 3] << 8) |
-                               ((unsigned char)payload[idx + 2]));
+            auto gyroy = short(((unsigned char)payload[idx + 1] << 8) |
+                               ((unsigned char)payload[idx + 0]));
             idx += 2;
-            auto gyroz = short(((unsigned char)payload[idx + 3] << 8) |
-                               ((unsigned char)payload[idx + 2]));
+            auto gyroz = short(((unsigned char)payload[idx + 1] << 8) |
+                               ((unsigned char)payload[idx + 0]));
             idx += 2;
-            auto temp = short(((unsigned char)payload[idx + 3] << 8) |
-                                ((unsigned char)payload[idx + 2]));
+            auto temp = short(((unsigned char)payload[idx + 1] << 8) |
+                                ((unsigned char)payload[idx + 0]));
             idx += 2;
             if(idx >= 512) {
                 QMessageBox::critical(0, "Error", "Count exceeds the number of entries in this block!");
